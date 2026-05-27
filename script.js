74410072fefe0950432c8fa8f649e29018d088ef
AASHIKA SINGH <aashikasingh24@navgurukul.org>
2026-05-28 00:24:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".mySwiper", {
+
+  loop: true,
+
+  speed: 1000,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  effect: "slide",
+
+});
